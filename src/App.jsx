05c4b401cf92dd402a521dfaa84afc91a030fc5e
54CHA <shsha.khv@gmail.com>
2025-08-24 09:54:45 +0300

    fixed build

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./index.css";
 import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import "toastr/build/toastr.min.css";
 import PolySaboteur from "./pages/PolySaboteur";
-import V2Page from "./pages/V2Page";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Toaster } from "sonner";
@@ -33,51 +32,37 @@ function App() {
 
   return (
     <Router>
-      <AppContent 
-        isHidden={isHidden}
-        isLoading={isLoading}
-        handleKeyDown={handleKeyDown}
-      />
-    </Router>
-  );
-}
-
-function AppContent({ isHidden, isLoading, handleKeyDown }) {
-  const location = useLocation();
-  const isV2 = location.pathname === '/v2';
-
-  return (
-    <div
-      className="min-h-screen flex flex-col relative"
-      onKeyDown={handleKeyDown}
-      tabIndex={0}
-    >
-      {isLoading && <Loader />}
       <div
-        className={cn(
-          "fixed inset-0 transition-opacity duration-100",
-          "bg-[Canvas]",
-          "z-[9999]",
-          isHidden ? "opacity-100" : "opacity-0 pointer-events-none"
-        )}
-      />
-      {!isV2 && <div className="fixed inset-0 -z-10 bg-background/80 backdrop-blur-xl" />}
-      <Navbar />
-      <div className={cn("flex-1", isV2 ? "pt-0" : "")}>
-        <Routes>
-          <Route path="/" element={<PolySaboteur />} />
-          <Route path="/v2" element={<V2Page />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        className="min-h-screen flex flex-col relative"
+        onKeyDown={handleKeyDown}
+        tabIndex={0}
+      >
+        {isLoading && <Loader />}
+        <div
+          className={cn(
+            "fixed inset-0 transition-opacity duration-100",
+            "bg-[Canvas]",
+            "z-[9999]",
+            isHidden ? "opacity-100" : "opacity-0 pointer-events-none"
+          )}
+        />
+        <div className="fixed inset-0 -z-10 bg-background/80 backdrop-blur-xl" />
+        <Navbar />
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<PolySaboteur />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+        <Footer />
+        <CustomToaster
+          position="top-center"
+          richColors
+          duration={3000}
+          isHidden={isHidden}
+        />
       </div>
-      {!isV2 && <Footer />}
-      <CustomToaster
-        position="top-center"
-        richColors
-        duration={3000}
-        isHidden={isHidden}
-      />
-    </div>
+    </Router>
   );
 }
 
